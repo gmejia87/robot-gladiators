ffbf4aeced26d7878ab5df749a371ee96a33fde6
G Codes <graciela.mejia1@yahoo.com>
2022-07-22 15:48:45 -0700

    some adds

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,3 +27,20 @@ console.log("Our robot's name is " + playerName);
 // Note the lack of quatation marks around playerName
 
 window.alert(playerName);
+
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
