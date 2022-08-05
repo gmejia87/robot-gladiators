9d45c18cdf8f8b5b8c2704ebbb3a56ebc968141d
G Codes <graciela.mejia1@yahoo.com>
2022-08-04 23:21:50 -0700

    shop input updated

--- assets/js/game.js
+++ assets/js/game.js
@@ -209,17 +209,18 @@ var endGame = function () {
 var shop = function () {
     //ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
 
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
     //use switch to carry out action
-    debugger;
     switch (shopOptionPrompt) {
         case 1:
             playerInfo.refillHealth();
             break;
         case 2:
-            playerInfo,upgradeAttack();
+            playerInfo.upgradeAttack();
             break;
         case 3:
             window.alert("Leaving the store.");
